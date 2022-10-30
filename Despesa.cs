6e146d2bc2eb0885988c06034ffb13acb041a7e9
jonatasx <jonatas.santospessoa@gmail.com>
2022-10-30 20:49:01 -0300

    garantindo envio

---
+++ Despesa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edka.Sa04
+{
+    internal class Despesa
+    {
+
+        public string Descricao { get; set; }
+
+        public decimal Valor { get; set; }
+
+        public DateTime Vencimento { get; set; }
+
+        //Polimorfismo
+        //Override
+        //Método no pai que é sobrescrito pelo filho
+
+        public override string ToString ()=>
+             $"{Descricao,-20}-{Vencimento:dd/MM/yyyy)}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace Edka.Sa04
+    {
+        internal class Program
+        {
+            public static List<Despesa> despesas = new List<Despesa>
+            {
+                //Lista Genérica < >
+                new Despesa{Descricao="Internet",Valor=89,Vencimento= new DateTime(2022,11,15)},
+                new Despesa{Descricao="Luz",Valor=110,Vencimento= new DateTime(2022,11,07)},
+                new Despesa{Descricao="Água",Valor=70,Vencimento= new DateTime(2022,11,02)}
+
+            };
+            static void Main(string[] args)
+            {
+                //Filtro LINQ
+                //from
+                //in
+                //select
+
+                var despesasAbaixodeCem = from d in despesas select d;
+
+                foreach(var item in despesasAbaixodeCem)
+                {
+                Console.WriteLine(item);
+    
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
